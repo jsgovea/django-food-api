9438c2967b852757d1ad40ce504f6b3ce676ce75
Jorge <jorgealejandro2116@gmail.com>
2020-01-01 21:58:08 -0800

    Heroku deploy

--- food/settings.py
+++ food/settings.py
@@ -135,6 +135,11 @@ AUTHENTICATION_BACKENDS = {
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
