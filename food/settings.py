3f622933175dbf935417cfb53f042ddb451b5863
Jorge <jorgealejandro2116@gmail.com>
2020-01-01 23:25:51 -0800

    host

--- food/settings.py
+++ food/settings.py
@@ -136,6 +136,9 @@ AUTHENTICATION_BACKENDS = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
