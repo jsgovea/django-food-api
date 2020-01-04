769eedfbc4bf681c652c36942f7628035f33dca4
Jorge <jorgealejandro2116@gmail.com>
2020-01-04 00:23:36 -0800

    fix

--- food/wsgi.py
+++ food/wsgi.py
@@ -14,6 +14,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'food.settings.dev')
 application = get_wsgi_application()
 # application = DjangoWhiteNoise(application)
